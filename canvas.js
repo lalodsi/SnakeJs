5ce7ddb6f869236a2ee1fcd41f6c1f6238deffed
Luis <lalo_luis_6@hotmail.com>
2021-04-17 01:16:28 -0500

    Cola de la serpiente

--- canvas.js
+++ canvas.js
@@ -3,40 +3,46 @@ var canvas = document.getElementById("game");
 var pincel = canvas.getContext("2d");
 document.addEventListener("keydown",teclaPulsada);
 //Variables
+const maxSize = 10;
 var SquareSize = 20;
-var posX = 0, posY = 0;
+var posX = [0, 0, 0], posY = [0, 0, 0];
 var movX = 0, movY = 0;
 var saltos = 20;
+var length = 5;
 //Game
 setInterval(game,1000/15);
 
 function game(){
-	if ((movX+movY)!= 0){
-		posX += movX;
-		posY += movY;
-		movX = 0;
-		movY = 0;
+	for (let i = length-2; i >= 0; i--) {
+		posY[i+1] = posY[i];
+		posX[i+1] = posX[i];
 	}
-	
-	if (posX < 0){
-		posX = 0;
+	if ( 0 != (movX+movY) ){
+		posX[0] += movX;
+		posY[0] += movY;
 	}
-	if (posX > canvas.width/SquareSize-1){
-		posX = canvas.width/SquareSize-1;
+	if (posX[0] < 0){
+		posX[0] = 0;
 	}
-	if (posY < 0){
-		posY = 0;
+	if (posX[0] > canvas.width/SquareSize-1){
+		posX[0] = canvas.width/SquareSize-1;
 	}
-	if (posY > canvas.height/SquareSize-1){
-		posY = canvas.height/SquareSize-1;
+	if (posY[0] < 0){
+		posY[0] = 0;
 	}
-
+	if (posY[0] > canvas.height/SquareSize-1){
+		posY[0] = canvas.height/SquareSize-1;
+	}
 	//Fondo
 	pincel.fillStyle = "black";
 	pincel.fillRect(0,0,canvas.width,canvas.height);
 	//Jugador
-	pincel.fillStyle = "blue";
-	pincel.fillRect(posX*saltos,posY*saltos,SquareSize,SquareSize);
+	for (let i = 0; i < posY.length; i++) {
+		pincel.fillStyle = "blue";
+		pincel.fillRect(posX[i]*saltos,posY[i]*saltos,SquareSize-2,SquareSize-2);
+	}
+	// posX.unshift(posX.pop());
+	// posY.unshift(posY.pop());
 	//
 }
 
